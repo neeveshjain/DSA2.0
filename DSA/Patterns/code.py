15d0cbe43ccdd7e46f2cf14a43a1fc590bf36f56
Neevesh Jain <neeveshjain27@gmail.com>
2024-06-12 08:23:41 +0530

    Question on Stack

--- DSA/Patterns/code.py
+++ DSA/Patterns/code.py
@@ -109,3 +109,85 @@
 #         print("*", end="")
 #     print()
 
+# -------------------------------------------------------
+
+# *
+# **
+# ***
+# ****
+# *****
+# ****
+# ***
+# **
+# *
+#
+# for i in range(5):
+#     for j in range(i):
+#         print('*',end='')
+#     print()
+# for k in range(5,0,-1):
+#     for l in range(k):
+#         print('*',end='')
+#     print()
+
+# -------------------------------------------------------
+
+# 1
+# 01
+# 101
+# 0101
+#
+# n = 5
+# for i in range(0,n):
+#     for j in range(0,i):
+#         if i == j :
+#             print(0,end='')
+#         elif (i%2 == 1 and j%2 == 0):
+#             print(1,end='')
+#         elif (i%2 == 0 and j%2 == 0):
+#             print(0,end='')
+#         elif (i%2 == 0 and j%2 == 1):
+#             print(1,end='')
+#         elif (i%2 == 1 and j%2 == 1):
+#             print(0 ,end='')
+#     print()
+#
+# #Optimised version
+# n = 5
+# for i in range(n):
+#     for j in range(i):
+#         # Determine the value to print based on the parity of i and j
+#         if (i % 2 == 0 and j % 2 == 0) or (i % 2 == 1 and j % 2 == 1):
+#             print(0, end='')
+#         else:
+#             print(1, end='')
+#     print()
+# -------------------------------------------------------
+
+# 1      1
+# 12    21
+# 123  321
+# 12344321
+# n = 5
+# for i in range(n):
+#     for j in range(1,i+1):
+#         print(j,end='')
+#     for k in range(2*(n-i-1)):
+#         print(' ',end='')
+#     for l in range(i,0,-1):
+#         print(l,end='')
+#     print()
+
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
+# n = 6
+# count = 1
+# for i in range(n):
+#     for j in range(i):
+#         print(count,end=' ')
+#         count += 1
+#     print()
+
